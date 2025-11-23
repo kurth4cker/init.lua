14917e0e7ebfafa94d65dc903eadaa55d57c606b
kurth4cker <kurth4cker@gmail.com>
2025-11-23 21:23:06 +0300

    colors: make background transparent

---
+++ after/plugin/colors.lua
@@ -0,0 +1,6 @@
+function Kurth4ckerSetupTransparentBackground()
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'None' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'None' })
+end
+
+Kurth4ckerSetupTransparentBackground()
